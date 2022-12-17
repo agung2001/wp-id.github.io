186a8273e2f6f54fca1ec4e4277bdfc58dcfe368
Agung Sundoro <muhagung2001@gmail.com>
2022-12-17 14:57:57 +0700

    Initial test sveltekit using adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs"
+		}),
+		paths: {
+			base: dev ? "" : "/wp-id.github.io",
+		}
 	}
 };
 
