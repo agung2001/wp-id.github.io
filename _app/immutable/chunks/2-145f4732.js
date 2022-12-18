ef8dfd7374f785225e9b1e962d36547552ba5727
agung2001 <muhagung2001@gmail.com>
2022-12-18 11:47:04 +0000

    Deploying to gh-pages from @ agung2001/wp-id.github.io@39ed80525cf3f83953e622d76043289ebb1fe9c9 🚀

---
+++ _app/immutable/chunks/2-145f4732.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-d0aebc77.js";export{t as component};
---
+++ _app/immutable/chunks/config-7249d5f3.js
@@ -0,0 +1,1 @@
+const a={title:"Komunitas WordPress Indonesia Percobaan",description:"Komunitas WordPress Indonesia | Kabar Terbaru, Kegiatan dan Panduan WordPress",APIURL:"https://wp-id.org/wp-json/wp/v2"};export{a as c};
