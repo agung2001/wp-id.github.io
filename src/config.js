0be825fb601edc164ab4c0ba31885c76b3cc6331
Agung Sundoro <muhagung2001@gmail.com>
2022-12-18 18:52:43 +0700

    Revert config.js to original

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  title: "Komunitas WordPress Indonesia Percobaan",
+  title: "Komunitas WordPress Indonesia",
   description: "Komunitas WordPress Indonesia | Kabar Terbaru, Kegiatan dan Panduan WordPress",
   APIURL: "https://wp-id.org/wp-json/wp/v2"
 }
