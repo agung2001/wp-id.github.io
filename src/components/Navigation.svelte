356f9e41672ad881c00d68771b490b739c27225d
Agung Sundoro <muhagung2001@gmail.com>
2022-12-20 08:42:07 +0700

    HOTFIX: Navigation dropdown trigger

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -98,14 +98,18 @@
                         class:hover:bg-slate-200={ActiveMenu.includes('repository')}
                         class:hover:text-black={ActiveMenu.includes('repository')}
                         on:click={() => { dropdowns.repository = !dropdowns.repository }}
+                        on:mouseenter={() => { dropdowns.repository = true }}
+                        on:mouseleave={() => { dropdowns.repository = false }}
                     >
                         <i class="fa-solid fa-code-compare pt-1"></i>
                         Repository
                         <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </div>
                     <!-- Dropdown menu -->
-                    <div class="absolute z-10 mt-4 font-normal bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+                    <div class="absolute z-10 mt-4 md:mt-0 font-normal bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
                          class:hidden={!dropdowns.repository}
+                         on:mouseenter={() => { dropdowns.repository = true }}
+                         on:mouseleave={() => { dropdowns.repository = false }}
                     >
                         <ul class="py-1  text-gray-700 dark:text-gray-400">
                             <li>
